2c0d82a9bd5bc49bf757e7348b6c2d217f434e89
Christopher Lytle <lytle.chris88@gmail.com>
2023-02-26 16:59:32 -0600

    Initial

---
+++ server/conf/settings.py
@@ -0,0 +1,74 @@
+r"""
+Evennia settings file.
+
+The available options are found in the default settings file found
+here:
+
+/usr/src/evennia/evennia/settings_default.py
+
+Remember:
+
+Don't copy more from the default file than you actually intend to
+change; this will make sure that you don't overload upstream updates
+unnecessarily.
+
+When changing a setting requiring a file system path (like
+path/to/actual/file.py), use GAME_DIR and EVENNIA_DIR to reference
+your game folder and the Evennia library folders respectively. Python
+paths (path.to.module) should be given relative to the game's root
+folder (typeclasses.foo) whereas paths within the Evennia library
+needs to be given explicitly (evennia.foo).
+
+If you want to share your game dir, including its settings, you can
+put secret game- or server-specific settings in secret_settings.py.
+
+"""
+
+# Use the defaults from Evennia unless explicitly overridden
+from evennia.settings_default import *
+
+######################################################################
+# Evennia base server config
+######################################################################
+
+# This is the name of your game. Make it catchy!
+SERVERNAME = "mygame"
+
+
+######################################################################
+# Settings given in secret_settings.py override those in this file.
+######################################################################
+try:
+    from server.conf.secret_settings import *
+except ImportError:
+    print("secret_settings.py file not found or failed to import.")
+
+######################################################################
+# Evennia Database config
+######################################################################
+
+# Database config syntax:
+# ENGINE - path to the the database backend. Possible choices are:
+#            'django.db.backends.sqlite3', (default)
+#            'django.db.backends.mysql',
+#            'django.db.backends.postgresql',
+#            'django.db.backends.oracle' (untested).
+# NAME - database name, or path to the db file for sqlite3
+# USER - db admin (unused in sqlite3)
+# PASSWORD - db admin password (unused in sqlite3)
+# HOST - empty string is localhost (unused in sqlite3)
+# PORT - empty string defaults to localhost (unused in sqlite3)
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "evennia",
+        "USER": "evennia",
+        "PASSWORD": "evennia1234",
+        "HOST": "10.0.10.3",
+        "PORT": "3306",
+    }
+}
+
+AUTO_CREATE_CHARACTER_WITH_ACCOUNT = False
+AUTO_PUPPET_ON_LOGIN = False
+CHARGEN_MENU = "world.chargen_menu"
